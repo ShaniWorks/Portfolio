61ae7e601c5b02a442001d5dbcc6809ff891f846
Zeeshan Haider <zeeshanhaiderrajpoot@gmail.com>
2024-09-04 16:19:09 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,28 @@
 let menubar = document.querySelector('#menu');
 let navbar = document.querySelector('.navbar');
+
 menubar.onclick = () => {
     menubar.classList.toggle('bx-x');
     navbar.classList.toggle('active');
 }
+
+// Animation for text "I'm Zeeshan Haider, Senior E-Commerce Officer"
+const animatedText = document.querySelector('.text-animation h2');
+const text = "I'm Zeeshan Haider, Senior E-Commerce Officer";
+let index = 0;
+
+function animateText() {
+    if (index < text.length) {
+        animatedText.textContent += text.charAt(index);
+        index++;
+        setTimeout(animateText, 100);
+    } else {
+        setTimeout(() => {
+            animatedText.textContent = '';
+            index = 0;
+            animateText();
+        }, 3000);
+    }
+}
+
+window.onload = animateText;
