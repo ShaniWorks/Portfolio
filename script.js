ad91ef988e941bef5057ccd45366772c1ce333ea
Zeeshan Haider <zeeshanhaiderrajpoot@gmail.com>
2024-09-05 03:16:40 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,16 +1,8 @@
-let menubar = document.querySelector('#menu');
-let navbar = document.querySelector('.navbar');
-
-menubar.onclick = () => {
-    menubar.classList.toggle('bx-x');
-    navbar.classList.toggle('active');
-};
-
-// Header hide on scroll
+// Header Hide on Scroll
 let prevScrollPos = window.pageYOffset;
 const header = document.querySelector('.header');
 
-window.onscroll = function() {
+window.onscroll = function () {
     let currentScrollPos = window.pageYOffset;
     if (prevScrollPos > currentScrollPos) {
         header.classList.remove('hidden');
@@ -20,3 +12,27 @@ window.onscroll = function() {
     prevScrollPos = currentScrollPos;
 };
 
+// Mobile Menu Toggle
+const menuIcon = document.getElementById('menu');
+const navbar = document.querySelector('.navbar');
+
+menuIcon.onclick = () => {
+    navbar.classList.toggle('active');
+};
+
+// Text Typing Animation
+const textAnimation = document.querySelector('.text-animation h2');
+const textContent = "I'm Zeeshan Haider Senior E-Commerce Officer";
+let i = 0;
+
+function typeWriter() {
+    if (i < textContent.length) {
+        textAnimation.textContent += textContent.charAt(i);
+        i++;
+        setTimeout(typeWriter, 100); // Adjust typing speed by changing the delay time
+    }
+}
+
+window.onload = () => {
+    typeWriter(); // Start the typing animation on page load
+};
