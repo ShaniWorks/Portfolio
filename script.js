42e5ceec8db338bf1afcb53276c2a61b5415567b
Zeeshan Haider <zeeshanhaiderrajpoot@gmail.com>
2024-09-05 03:36:05 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,16 +1,6 @@
 let menubar = document.querySelector('#menu');
 let navbar = document.querySelector('.navbar');
-let header = document.querySelector('.header');
-
 menubar.onclick = () => {
     menubar.classList.toggle('bx-x');
     navbar.classList.toggle('active');
-};
-
-window.onscroll = () => {
-    if (window.scrollY > 50) {
-        header.classList.add('scrolled');
-    } else {
-        header.classList.remove('scrolled');
-    }
-};
+}
