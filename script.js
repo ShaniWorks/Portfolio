4a55b74621875b1d2a19d837bb053c794de3c8a5
Zeeshan Haider <zeeshanhaiderrajpoot@gmail.com>
2024-09-04 15:49:09 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,20 +1,6 @@
-// Toggle the menu icon and navigation bar on click
-let menubar = document.querySelector('#menu');
+let menu = document.querySelector('#menu');
 let navbar = document.querySelector('.navbar');
 
-menubar.onclick = function() {
-    menubar.classList.toggle('bx-x');
+menu.onclick = () => {
     navbar.classList.toggle('active');
 };
-
-// Optional: Close the navbar when a link is clicked (useful for mobile view)
-let navLinks = document.querySelectorAll('.navbar a');
-
-navLinks.forEach(function(link) {
-    link.onclick = function() {
-        menubar.classList.remove('bx-x');
-        navbar.classList.remove('active');
-    };
-});
-
-console.log("JavaScript is loaded and working correctly.");
