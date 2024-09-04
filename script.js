1b2283eb15fc4ecc85a6f8f3abd5be48755ef766
Zeeshan Haider <zeeshanhaiderrajpoot@gmail.com>
2024-09-04 15:55:29 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
-let menu = document.querySelector('#menu');
+let menubar = document.querySelector('#menu');
 let navbar = document.querySelector('.navbar');
 
-menu.onclick = () => {
+menubar.onclick = () => {
+    menubar.classList.toggle('bx-x');
     navbar.classList.toggle('active');
-};
+}
